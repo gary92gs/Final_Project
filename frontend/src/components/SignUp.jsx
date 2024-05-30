504f2442b82b545fec0afc3fabc55ff259b5aa7e
LiamGunning07 <liamgunning07@gmail.com>
2024-05-30 11:53:49 -0600

    Add boilerplate for SignUp

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function SignUp() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
   return (
     <div className="SignUp">
       <h1 className='title'> Portfolio Prophet </h1>
         <div className='signup_container'>
+          <h2>Create an Account now!</h2>
+          <form>
+            <label htmlFor='username'>Username:</label>
+            <input
+            type="text"
+            id="username"
+            />
 
+            <label htmlFor='email'>Email:</label>
+            <input
+            type="text"
+            id="email"
+            />
+
+            <label htmlFor='password'>Password:</label>
+            <input
+            type="password"
+            id="password"
+            />
+
+            <label htmlFor='password-confirmation'>Password Confirmation:</label>
+            <input
+            type="text"
+            id="password-confirmation"
+            />
+          </form>
+          <button type='submit'>Sign Up</button>
+          <p>Already have an account? Login Here(link)</p>
         </div>
     </div>
   );
