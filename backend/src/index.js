7c323bf5c13b98302bf250ba09b93648befdb54a
gary92gs <gary92.gs@gmail.com>
2024-05-30 10:15:21 -0600

    adds one more route

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,7 @@ app.use(express.json());
 //IMPORT ROUTES
 // const usersRoutes = require('./routes/users');
 // const searchRoutes = require('./routes/search');
+// const saveStockRoutes = require('./routes/saveStock');
 // const stockOverviewRoutes = require('./routes/stockOverview');
 // const dashboardAnalysisRoutes = require('./routes/dashboardAnalysis');
 // const favouritesRoutes = require('./routes/favourites');
@@ -26,6 +27,7 @@ app.use(express.json());
 //MOUNT ROUTES
 // app.use('/api/users', usersRoutes);
 // app.use('/api/search', searchRoutes);
+// app.use('/api/save-stock', saveStockRoutes);
 // app.use('/api/stock-overview', stockOverviewRoutes);
 // app.use('/api/dashboard-analysis', dashboardAnalysisRoutes);
 // app.use('/api/favourites', favouritesRoutes);
