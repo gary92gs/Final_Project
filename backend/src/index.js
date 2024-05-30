6dc2a4ae7c3beed5b8d53b2c50c9ad7a17aa0720
gary92gs <gary92.gs@gmail.com>
2024-05-30 00:53:23 -0600

    adds backend packages. initializes basic server and middleware. implements environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,36 @@
-// EXPRESS APP INDEX GOES HERE
+//IMPORT ENV VARIABLES
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+
+//IMPORTS
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require("cors");
+
+//INITIALIZE SERVER OBJ
+const app = express();
+
+//MOUNT MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//IMPORT ROUTES
+// const usersRoutes = require('./routes/users');
+// const searchRoutes = require('./routes/search');
+// const stockOverviewRoutes = require('./routes/stockOverview');
+// const dashboardAnalysisRoutes = require('./routes/dashboardAnalysis');
+// const favouritesRoutes = require('./routes/favourites');
+
+//MOUNT ROUTES
+// app.use('/api/users', usersRoutes);
+// app.use('/api/search', searchRoutes);
+// app.use('/api/stock-overview', stockOverviewRoutes);
+// app.use('/api/dashboard-analysis', dashboardAnalysisRoutes);
+// app.use('/api/favourites', favouritesRoutes);
+
+//START SERVER
+app.listen(PORT, () => {
+  console.log(`API Server is listening on port ${PORT}`);
+});
