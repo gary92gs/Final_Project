16e64a86ae52baedbcf19537cf8828a5533a0cfa
LiamGunning07 <liamgunning07@gmail.com>
2024-05-31 15:01:38 -0600

    Installed react-router-dom and implemented into app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import SignUp from './components/SignUp';
 import TopNavBar from './components/TopNavBar';
+import Login from './components/Login'
 import "./styles/global.css"
+import {BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <TopNavBar/>
-      <SignUp/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={
+          <div className="App">
+            <TopNavBar />
+            {/* <Home /> */}
+            {/* Add other components you want in the home page layout here */}
+          </div>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
