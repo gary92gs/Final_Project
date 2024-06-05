d2dd31532fbc12ab88e555a01316885c63b42e41
LiamGunning07 <liamgunning07@gmail.com>
2024-06-05 10:51:21 -0600

    Config and Setup for Chart.js/Added some styles for the chart

--- frontend/src/components/SelectedStock.jsx
+++ frontend/src/components/SelectedStock.jsx
@@ -4,9 +4,9 @@ import ItemFavButton from './ItemFavButton'
 import '../styles/SelectedStock.css'
 import { useState } from 'react'
 import WatchlistSidebar from './WatchlistSidebar'
+import { Graph } from './Graph'
 
 function SelectedStock() {
-
 
   const favStocks = [
     {
@@ -58,6 +58,7 @@ const {selectedStock, setSelectedStock} = useState()
           <h2> Stock Description/Summary </h2>
           <p> Apple Inc. stands out for its commitment to innovation, quality, and design. Its diverse product line and ecosystem offer seamless integration and a superior user experience. From revolutionizing personal computing with the Macintosh to redefining the smartphone with the iPhone, Apple continues to shape the future of technology and consumer electronics. </p>
           {/* Chart.js */}
+          <Graph />
           <ItemFavButton />
         </div>
       </article>
