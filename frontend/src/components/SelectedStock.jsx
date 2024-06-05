64db282c942a0856c0da73d0c95efb15e5ad05c9
Kai Wingfield <kaiwingfield17@gmail.com>
2024-06-05 11:47:18 -0700

    change stylings for selected stock, nav bar, homepage, watchlist

--- frontend/src/components/SelectedStock.jsx
+++ frontend/src/components/SelectedStock.jsx
@@ -45,6 +45,7 @@ const {selectedStock, setSelectedStock} = useState()
   return(
     <div>
       <TopNavBar/>
+      <div className='selected-page'>
       <div className='selected-container'>
       <WatchlistSidebar favStocks={favStocks}/>
       <article className='stock-article'> 
@@ -53,15 +54,16 @@ const {selectedStock, setSelectedStock} = useState()
           <h1> Stock Title </h1>
           <span>2009 - 2024 </span>
         </div>
+        <ItemFavButton />
         <div className='stock-info'>
         <DataTable/>
           <h2> Stock Description/Summary </h2>
           <p> Apple Inc. stands out for its commitment to innovation, quality, and design. Its diverse product line and ecosystem offer seamless integration and a superior user experience. From revolutionizing personal computing with the Macintosh to redefining the smartphone with the iPhone, Apple continues to shape the future of technology and consumer electronics. </p>
           {/* Chart.js */}
           <Graph />
-          <ItemFavButton />
         </div>
       </article>
+      </div>
       </div>
     </div>
 
