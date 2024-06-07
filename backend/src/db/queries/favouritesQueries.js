38167318ae39d7b50c2e6302e6a3ca22d143cf10
Shadrak Lagadi <shadrl19@gmail.com>
2024-06-07 15:38:20 -0600

    Adds query function and route for deletion. Adds refactoring.

--- backend/src/db/queries/favouritesQueries.js
+++ backend/src/db/queries/favouritesQueries.js
@@ -1,7 +1,7 @@
 const db = require('./../index');
 
 const getUsersFavouriteStocks = async (userID) => {
-  
+
   const queryStr = `
   SELECT *
   FROM favourite_stocks
@@ -11,6 +11,9 @@ const getUsersFavouriteStocks = async (userID) => {
 
   try {
     const result = await db.query(queryStr, [userID]);
+    if (!result.rows.length) {
+      return false;
+    }
     return result.rows;
   }
   catch (error) {
@@ -22,15 +25,36 @@ const addStocktoUserFavourites = async (userID, stockID) => {
 
   const queryStr = `
   INSERT INTO favourite_stocks (user_id, stock_id)
-  VALUES (?, ?)
+  VALUES ($1, $2)
+  RETURNING *;
   `;
 
   try {
-    await db.query(queryStr, [userID, stockID])
+    await db.query(queryStr, [userID, stockID]);
+    if (!result.rows.length) {
+      return false;
+    }
+    return result.rows[0]
   }
   catch (error) {
     throw error;
   }
 }
 
-module.exports = { getUsersFavouriteStocks, addStocktoUserFavourites };
+const deleteStockFromUsersFavourites = async (userID, stockID) => {
+
+  const queryStr = `
+    DELETE FROM favourite_stocks
+    WHERE user_id = $1 AND stock_id = $2;
+  `;
+
+  try {
+    await db.query(queryStr, [userID, StockID]);
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = { getUsersFavouriteStocks, addStocktoUserFavourites, deleteStockFromUsersFavourites };
--- backend/src/routes/favourites.js
+++ backend/src/routes/favourites.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getUsersFavouriteStocks, addStocktoUserFavourites }  = require('../db/queries/favouritesQueries')
+const { getUsersFavouriteStocks, addStocktoUserFavourites, deleteStockFromUsersFavourites } = require('../db/queries/favouritesQueries')
+const { getUserSessionCookie } = require('../helpers/userSessionHelpers')
 
 router.get('/', async (req, res) => {
   // get user ID from session
   try {
-    const userID = req.session.userID;
+    const userID = getUserSessionCookie(req);
     if (!userID) {
-      return res.status(401).json({ message: 'Unauthorized, please leave' });
+      return res.status(401).json({ message: 'Unauthorized' });
     }
     // find users favourited stocks
     // extract users favourited stocks
     const favouriteStocks = await getUsersFavouriteStocks(userID);
     // send the favourites object as a response to component
-    res.json({ favourites: favouriteStocks })
+    res.json({ userFavourites: favouriteStocks })
   } catch (error) {
     console.log(`Error: ${error}`);
     res.status(500).json({ message: 'Internal Server Error' })
@@ -22,18 +23,22 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // get user ID from session
+    // get user ID from session
   try {
-    const userID = req.session.userID;
+    const userID = getUserSessionCookie(req);
     if (!userID) {
-      return res.status(401).json({ message: 'Unauthorized, please leave' });
+      return res.status(401).json({ message: 'Unauthorized' });
     }
-  // stock ID extraction from request body, frontend reference
-  const stockID = req.body.stockID;
+    // stock ID extraction from request body, frontend reference
+    const stockID = req.body.stockID;
+    // query db to remove stock from favourite
+    const stockAddedtoFavourites = await addStocktoUserFavourites(userID, stockID);
 
-  await addStocktoUserFavourites(userID, stockID);
-  // send server success message
-  res.json({ message: 'Stonk has been added to your favourites' });
+    if (!stockAddedtoFavourites) {
+      return res.status(500).json({ message: 'Adding stocks to favourites was unsuccessful' })
+    }
+    // send server success message
+    return res.status(200).json({ message: 'Stock has been added to your favourites' });
   }
   catch (error) {
     console.log(`Error: ${error}`);
@@ -41,8 +46,24 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/', () => {
-
+router.delete('/', async () => {
+  // get user ID from session
+  try {
+    const userID = getUserSessionCookie(req);
+    if (!userID) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    // stock ID extraction from request body, frontend reference
+    const stockID = req.body.stockID;
+    // query db to remove stock from favourite
+    await deleteStockFromUsersFavourites(userID, stockID);
+    // send server success message
+    res.json({ message: 'Stonk has been deleted from your favourites' })
+  }
+  catch (error) {
+    console.log(`Error: ${error}`);
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
 })
 
 module.exports = router;
