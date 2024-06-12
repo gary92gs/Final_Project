882fb6d085d29862c89aa8a1bd7faab1e4ea3a7d
Kai Wingfield <kaiwingfield17@gmail.com>
2024-06-12 10:49:51 -0700

    Polish frontend styling

--- frontend/src/components/WatchlistMainItem.jsx
+++ frontend/src/components/WatchlistMainItem.jsx
@@ -13,12 +13,14 @@ const WatchlistMainItem = ({
     <div className="watchlist-main-item" onClick={() => fetchSelectedStockData(tickerSymbol)}>
       <img src={favStock.image_url} alt={`${favStock.company_name} logo`} className="stock-image" />
       <div className="stock-details">
-        <h2> {favStock.ticker_symbol} </h2>
-        <h3> {favStock.company_name} </h3>
+        <div className='watchlist-main-item-headers'>    
+          <h2> {favStock.ticker_symbol} </h2>
+          <h3> {favStock.company_name} </h3>
+          <h4>Country: {favStock.country}</h4> 
+        </div>
+        <div className="stock-description">
         <p> {favStock.description} </p>
-        <p><strong>Sector:</strong> {favStock.sector}</p>
-        <p><strong>Country:</strong> {favStock.country}</p>
-        <p><strong>Currency:</strong> {favStock.currency}</p>
+        </div>
       </div>
     </div>
   );
