7b9a7f2fcd28c4a89816ffcbefd37091e060840d
Kai Wingfield <kaiwingfield17@gmail.com>
2024-06-13 13:29:14 -0700

    Implement dataTable and start styling

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -1,123 +1,42 @@
-import React from 'react';
-import '../styles/DataTable.css'
-import "../styles/global.css"
-import ReactDataTable from 'react-data-table-component';
+import React from "react";
+import "../styles/DataTable.css";
+import "../styles/global.css";
+
 
 function DataTable({ stockData }) {
-
-  const columns = [
-    {
-      name: 'Title',
-      selector: row => row.title,
-    },
-    {
-      name: 'Year',
-      selector: row => row.year,
-    },
-    {
-      name: '3',
-      selector: row => row.title,
-    },
-    {
-      name: '4',
-      selector: row => row.title,
-    },
-    {
-      name: '5',
-      selector: row => row.title,
-    },
-    {
-      name: '6',
-      selector: row => row.title,
-    },
-    {
-      name: '7',
-      selector: row => row.title,
-    },
-    {
-      name: '8',
-      selector: row => row.title,
-    },
-    {
-      name: '9',
-      selector: row => row.title,
-    },
-  ];
-  
-  const data = [
-      {
-      id: 1,
-      title: 'Hi',
-      year: '1988',
-    },
-    {
-      id: 2,
-      title: 'Hello',
-      year: '1984',
-    },
-  ]
+  const dataRows = stockData.historical_data.map((dataRow, index) => {
+    return (
+      <>
+        <div className="data-row-value">{dataRow.report_year}</div>
+        <div className="data-row-value report-quarter">{dataRow.report_quarter}</div>
+        <div className="data-row-value">{dataRow.quarterly_price_median}</div>
+        <div className="data-row-value">{dataRow.book_value}</div>
+        <div className="data-row-value net-income">{dataRow.net_income}</div>
+        <div className="data-row-value outstanding-shares">{dataRow.outstanding_shares}</div>
+        <div className="data-row-value shareholders-equity">{dataRow.shareholders_equity}</div>
+        <div className="data-row-value">{dataRow.reported_eps}</div>
+        <div className="data-row-value">{dataRow.quarterly_dividend}</div>
+        <div className="data-row-value">{dataRow.total_dividend_payout}</div>
+      </>
+    )
+  });
   return (
-    <div className='data-table-container'>
-		 <ReactDataTable
-		 	columns={columns}
-		 	data={data}
-		 />
-    </div>
-	);
-};
-  
-export default DataTable;
+    <div className='data-table'>
 
-// <div className='data-table'>
-//   <h2>Stock Stats</h2>
-//   <table className='styled-table'>
-//     <thead>
-//       <tr className='active-row'>
-//         <th>Header 1</th>
-//         <th>Header 2</th>
-//         <th>Header 3</th>
-//         <th>Header 4</th>
-//         <th>Header 5</th>
-//         <th>Header 6</th>
-//         <th>Header 7</th>
-//         <th>Header 8</th>
-//         <th>Header 9</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//     <tr className='active-row'>
-//         <td>Row 1, Cell 1</td>
-//         <td>Row 1, Cell 2</td>
-//         <td>Row 1, Cell 3</td>
-//         <td>Row 1, Cell 4</td>
-//         <td>Row 1, Cell 5</td>
-//         <td>Row 1, Cell 6</td>
-//         <td>Row 1, Cell 7</td>
-//         <td>Row 1, Cell 8</td>
-//         <td>Row 1, Cell 9</td>
-//       </tr>
-//       <tr className='active-row'>
-//         <td>Row 2, Cell 1</td>
-//         <td>Row 2, Cell 2</td>
-//         <td>Row 2, Cell 3</td>
-//         <td>Row 2, Cell 4</td>
-//         <td>Row 2, Cell 5</td>
-//         <td>Row 2, Cell 6</td>
-//         <td>Row 2, Cell 7</td>
-//         <td>Row 2, Cell 8</td>
-//         <td>Row 2, Cell 9</td>
-//       </tr>
-//       <tr className='active-row'>
-//         <td>Row 3, Cell 1</td>
-//         <td>Row 3, Cell 2</td>
-//         <td>Row 3, Cell 3</td>
-//         <td>Row 3, Cell 4</td>
-//         <td>Row 3, Cell 5</td>
-//         <td>Row 3, Cell 6</td>
-//         <td>Row 3, Cell 7</td>
-//         <td>Row 3, Cell 8</td>
-//         <td>Row 3, Cell 9</td>
-//       </tr>
-//     </tbody>
-//   </table>
-// </div>
+        <div className="data-row-value">Year</div>
+        <div className="data-row-value report-quarter">Quarter</div>
+        <div className="data-row-value">Price Median</div>
+        <div className="data-row-value">Book Value</div>
+        <div className="data-row-value">Net Income</div>
+        <div className="data-row-value">Otsd. Shares</div>
+        <div className="data-row-value">Shld. Equity</div>
+        <div className="data-row-value">EPS</div>
+        <div className="data-row-value">Div/Share</div>
+        <div className="data-row-value">Total Div</div>
+
+      {dataRows}
+    </div>
+  )
+}
+
+export default DataTable;
