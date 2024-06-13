bc70f4eb68e31da2348a1ca2f784ffd768df8bd6
Kai Wingfield <kaiwingfield17@gmail.com>
2024-06-13 13:43:47 -0700

    add alternating rows to datatable

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -2,41 +2,41 @@ import React from "react";
 import "../styles/DataTable.css";
 import "../styles/global.css";
 
-
 function DataTable({ stockData }) {
   const dataRows = stockData.historical_data.map((dataRow, index) => {
+    const rowClass = index % 2 === 0 ? 'even-row' : 'odd-row';
     return (
-      <>
-        <div className="data-row-value">{dataRow.report_year}</div>
-        <div className="data-row-value report-quarter">{dataRow.report_quarter}</div>
-        <div className="data-row-value">{dataRow.quarterly_price_median}</div>
-        <div className="data-row-value">{dataRow.book_value}</div>
-        <div className="data-row-value net-income">{dataRow.net_income}</div>
-        <div className="data-row-value outstanding-shares">{dataRow.outstanding_shares}</div>
-        <div className="data-row-value shareholders-equity">{dataRow.shareholders_equity}</div>
-        <div className="data-row-value">{dataRow.reported_eps}</div>
-        <div className="data-row-value">{dataRow.quarterly_dividend}</div>
-        <div className="data-row-value">{dataRow.total_dividend_payout}</div>
-      </>
-    )
+      <React.Fragment key={index}>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.report_year}</div>
+        <div className={`data-row-value report-quarter ${rowClass}`}>{dataRow.report_quarter}</div>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.quarterly_price_median}</div>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.book_value}</div>
+        <div className={`data-row-value net-income ${rowClass}`}>{dataRow.net_income}</div>
+        <div className={`data-row-value outstanding-shares ${rowClass}`}>{dataRow.outstanding_shares}</div>
+        <div className={`data-row-value shareholders-equity ${rowClass}`}>{dataRow.shareholders_equity}</div>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.reported_eps}</div>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.quarterly_dividend}</div>
+        <div className={`data-row-value ${rowClass}`}>{dataRow.total_dividend_payout}</div>
+      </React.Fragment>
+    );
   });
+
   return (
     <div className='data-table'>
-
-        <div className="data-row-value">Year</div>
-        <div className="data-row-value report-quarter">Quarter</div>
-        <div className="data-row-value">Price Median</div>
-        <div className="data-row-value">Book Value</div>
-        <div className="data-row-value">Net Income</div>
-        <div className="data-row-value">Otsd. Shares</div>
-        <div className="data-row-value">Shld. Equity</div>
-        <div className="data-row-value">EPS</div>
-        <div className="data-row-value">Div/Share</div>
-        <div className="data-row-value">Total Div</div>
+      <div className="data-row-value header">Year</div>
+      <div className="data-row-value header report-quarter">Quarter</div>
+      <div className="data-row-value header ">Price Median</div>
+      <div className="data-row-value header ">Book Value</div>
+      <div className="data-row-value header ">Net Income</div>
+      <div className="data-row-value header ">Otsd. Shares</div>
+      <div className="data-row-value header ">Shld. Equity</div>
+      <div className="data-row-value header ">EPS</div>
+      <div className="data-row-value header ">Div/Share</div>
+      <div className="data-row-value header ">Total Div</div>
 
       {dataRows}
     </div>
-  )
+  );
 }
 
 export default DataTable;
